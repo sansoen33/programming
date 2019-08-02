2029062805be61a156061a05ea1bf504966ddcf3
sansoen33 <52350391+sansoen33@users.noreply.github.com>
2019-08-02 23:21:44 +0700

    Add files via upload

---
+++ Exercise4_Sansoen_S1.py
@@ -0,0 +1,9 @@
+e=75
+f=80
+g=85
+h=90
+print("-------You Score-------")
+print("Foundation English",":",e)
+print("General Business",":",f)
+print("Introduction To Computer",":",g)
+print("Computer Programming",":",h)
---
+++ Exercise5_2_Sansoen_S.py
@@ -0,0 +1,4 @@
+Distance=float(input("Enter Distance(km)"))
+Time=float(input("Enter Time(h)"))
+print(Distance/Time"km/h")
+
